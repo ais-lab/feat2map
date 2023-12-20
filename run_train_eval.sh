3e77909ef24aad14953956c589365a8c5ec2a094
thuanaislab <thuan.aislab@gmail.com>
2023-12-20 19:11:10 +0900

    uncomment

--- run_train_eval.sh
+++ run_train_eval.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# python train.py --dataset 7scenes --scene office --config_file configs/configsV2.ini --model 2 --cudaid 0 --augment 0
-# python train.py --dataset 7scenes --scene office --config_file configs/configsV2.ini --model 2 --cudaid 0 --resume_optim 1 --augment 0
+python train.py --dataset 7scenes --scene office --config_file configs/configsV2.ini --model 2 --cudaid 0 --augment 0
+python train.py --dataset 7scenes --scene office --config_file configs/configsV2.ini --model 2 --cudaid 0 --resume_optim 1 --augment 0
 python eval.py --dataset 7scenes --scene office --config_file configs/configsV2.ini --model 2 --cudaid 0 --augment 0
 
 
